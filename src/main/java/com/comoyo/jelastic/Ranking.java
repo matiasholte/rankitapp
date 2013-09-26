0ab186dba4fccb48f0f4611028ab562920931523
Matias Holte <matias@comoyo.com>
2013-09-26 10:58:56 +0200

    test what happens on error

--- src/main/java/com/comoyo/jelastic/Ranking.java
+++ src/main/java/com/comoyo/jelastic/Ranking.java
@@ -11,7 +11,7 @@ public class Ranking extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
-        resp.getWriter().println("<h1>Full ranking </h1>");
+        resp.getWriter()println("<h1>Full ranking </h1>");
         resp.getWriter().print(PersistentStorage.getRankingList());
     }
 }
